2d067cffe7c73870fc00ac60376148fc17a9d892
Giampiero Specogna <giampiero@analogrepublic.com>
2023-04-01 12:54:42 -0400

    fix: env variable

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import { config } from 'dotenv'
-config()
 ;(async () => {
-  await mongoose.connect(process.env.MONGO_DB_URL)
+  config()
+  await mongoose.connect(process.env.MONGODB_URL)
   mongoose.Promise = global.Promise
 })()
